a8a6641d3033ad5ec6c540c8809b62585af922b0
Matthew Carter <phuded@gmail.com>
2014-06-10 09:59:56 +0100

    Json ignore nulls

--- WebPokerCommons/src/main/java/poker/domain/notification/Notification.groovy
+++ WebPokerCommons/src/main/java/poker/domain/notification/Notification.groovy
@@ -1,8 +1,11 @@
 package poker.domain.notification
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+
 /**
  * Created by matt on 06/06/2014.
  */
+@JsonSerialize(include=JsonSerialize.Inclusion.NON_NULL)
 class Notification {
 
   String gameId
