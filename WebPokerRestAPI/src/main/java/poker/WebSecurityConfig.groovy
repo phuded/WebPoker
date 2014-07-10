9b181caff506ffc131c25c0eeaf8704853faceaa
Matthew Carter <phuded@gmail.com>
2014-07-11 00:31:38 +0100

    Players resource not authenticated

--- WebPokerRestAPI/src/main/java/poker/WebSecurityConfig.groovy
+++ WebPokerRestAPI/src/main/java/poker/WebSecurityConfig.groovy
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -22,10 +23,17 @@ class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     PlayerRepository playerRepository
 
     @Override
+    public void configure(WebSecurity webSecurity) throws Exception
+    {
+        webSecurity
+            .ignoring()
+                .antMatchers("/players/**")
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/players/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
