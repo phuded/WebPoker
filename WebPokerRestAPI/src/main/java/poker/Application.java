e7f6845eeff6d4b4ea36fe352e32517250f21e01
Matthew Carter <phuded@gmail.com>
2015-03-08 03:17:17 +0100

    Updated to latest version!

--- WebPokerRestAPI/src/main/java/poker/Application.java
+++ WebPokerRestAPI/src/main/java/poker/Application.java
@@ -1,14 +1,11 @@
 package poker;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
-@Configuration
-@EnableAutoConfiguration
-@ComponentScan
+
+@SpringBootApplication
 @EnableMongoAuditing
 public class Application{
 
